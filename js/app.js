bb0b1fdca84e3736c0111a14f88b7e6c0731c439
David Ruiz <davidl.ruizc@gmail.com>
2018-08-12 19:43:15 -0500

    organizing code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
+//captura datos y crea nuevos inputs 
 function dataShow() {
     var data = document.getElementById("email").value;
 
     if (data != 0) {
         document.getElementById("box").style.display = "none";
         console.log(data);
-        // document.getElementById("but").style.display = "none";
+        document.getElementById("but").style.display = "none";
         for (var x = 0; x < data; x++) {
             console.log(x);
             var form = document.createElement("INPUT");
             form.setAttribute("type", "number");
-            form.setAttribute("placeholder", "Type a number");
+            form.setAttribute("placeholder", "Ingrese valor.");
             document.getElementById("inputs").appendChild(form);
         }
     }
+
 }
+
+//procedimiento con inputs generados 
