c4eade536a1c9cb048c40413931de1ffc208e21d
David Ruiz <davidl.ruizc@gmail.com>
2018-08-14 14:46:22 -0500

    finish

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function dataShow() {
             var form = document.createElement("INPUT");
             form.setAttribute("type", "number");
             form.setAttribute("placeholder", "Ingrese valor.");
-            form.setAttribute("id", "generated"+x);
+            form.setAttribute("id", "generated" + x);
             document.getElementById("inputs").appendChild(form);
         }
         document.getElementById("start").style.display = "block";
@@ -18,17 +18,57 @@ function dataShow() {
 }
 
 //procedimiento con inputs generados 
-function process(){
+function process() {
     var data = parseInt(document.getElementById("email").value);
     var promedio = 0;
     var newData;
-    console.log("calculandoo wuuu c: "+ data);
-    for(var i = 0; i < data; i++){
-        newData = parseInt(document.getElementById("generated"+i).value);      
-        promedio = promedio + newData;        
+    var sumatoria = 0;
+    var incertidumbreA;
+    var valor = [];
+    //array = 9     1    2      3    4      5     6     7     8      9
+    var student = [12.7, 4.30, 3.18, 2.78, 2.57, 2.23, 2.08, 2.02, 1.96];
+    for (var i = 0; i < data; i++) {
+        newData = parseInt(document.getElementById("generated" + i).value);
+        promedio = promedio + newData;
+        valor[i]= newData;
     }
-    var total = promedio / data;
-    document.getElementById("Vpromedio").innerHTML = " " +total;
-    document.getElementById("box2").style.display = "flex";
     
+    var total = promedio / data;
+    //Incertidumbre aleatoria
+    //varianza
+    for (var a = 0; a < data; a++) {
+        newData = parseInt(document.getElementById("generated" + a).value);
+        sumatoria = sumatoria + (Math.pow((newData - total), 2));
+
+    }
+    var varianza = Math.sqrt(sumatoria / (data - 1));
+    //student
+    if (data == 2) {
+        incertidumbreA = student[student.length - 9] * (varianza / (Math.sqrt(data, 2)));
+        document.getElementById("Vincertidumbre").innerHTML = " " + incertidumbreA;
+
+    }
+    if (data == 3) {
+        incertidumbreA = student[student.length - 8] * (varianza / (Math.sqrt(data, 2)));
+        document.getElementById("Vincertidumbre").innerHTML = " " + incertidumbreA;
+
+    }
+    if (data == 4) {
+        incertidumbreA = student[student.length - 7] * (varianza / (Math.sqrt(data, 2)));
+        document.getElementById("Vincertidumbre").innerHTML = " " + incertidumbreA;
+
+    }
+    if (data == 5) {
+        incertidumbreA = student[student.length - 6] * (varianza / (Math.sqrt(data, 2)));
+        document.getElementById("Vincertidumbre").innerHTML = " " + incertidumbreA;
+    }
+
+    //Es
+    var menor = Math.min.apply(null, valor);
+    var Es =  total / menor;
+    document.getElementById("VEs").innerHTML = " " + Es;
+
+    document.getElementById("Vpromedio").innerHTML = " " + total;
+    document.getElementById("box2").style.display = "flex";
+
 }
